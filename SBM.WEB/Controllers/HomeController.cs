6691d883b1b7b2567e30a8e0fd876696c83d32f2
MisSero <misha555835@gmail.com>
2023-09-02 14:56:03 +0300

    add views, frontend, controller

---
+++ SBM.WEB/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SBM.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- SBM.WEB/Program.cs
+++ SBM.WEB/Program.cs
@@ -5,9 +5,19 @@ namespace SBM.WEB
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+                name: "defatul",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
