159e20986d3d9dbb447a424ab6744a61e6f760d0
MisSero <misha555835@gmail.com>
2023-09-03 12:05:34 +0300

    update connection string, EntityBase; add migrations

--- SDM.DAL/Entities/EntityBase.cs
+++ SDM.DAL/Entities/EntityBase.cs
@@ -8,12 +8,12 @@ public abstract class EntityBase
 
 	public Guid Id { get; set; }
 	public virtual string Title { get; set; }
-	public virtual string Subtitle { get; set; }
+	public virtual string? Subtitle { get; set; }
 	public virtual string Text { get; set; }
-	public virtual string TitleImagePath { get; set; }
-	public virtual string MetaTitle { get; set; }
-	public virtual string MetaDescription{ get; set; }
-	public virtual string MetaKeywords { get; set; }
+	public virtual string? TitleImagePath { get; set; }
+	public virtual string? MetaTitle { get; set; }
+	public virtual string? MetaDescription{ get; set; }
+	public virtual string? MetaKeywords { get; set; }
 	[DataType(DataType.Time)]
 	public DateTime DateAdded { get; set; }
 }
