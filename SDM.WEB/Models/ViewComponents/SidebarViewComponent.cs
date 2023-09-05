a08f22abce1f952c2be39c94b60ad09771195159
MisSero <misha555835@gmail.com>
2023-09-05 11:29:14 +0300

    add sidebar

---
+++ SDM.WEB/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SDM.BLL.DTO;
+using SDM.BLL.Interfaces;
+
+namespace SDM.WEB.Models.ViewComponents;
+
+public class SidebarViewComponent : ViewComponent
+{
+    private readonly IServiceItemsService _itemsService;
+    private readonly IMapper _mapper;
+
+    public SidebarViewComponent(IServiceItemsService itemsService)
+    {
+        _itemsService = itemsService;
+        _mapper = new MapperConfiguration(cfg =>
+            cfg.CreateMap<ServiceItemDTO, ServiceItemViewModel>()).CreateMapper();
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var serviceItemsDTO = await _itemsService.GetAllServiceItems();
+
+        return await Task.FromResult((IViewComponentResult)View("Default",
+            _mapper.Map<List<ServiceItemDTO>, List<ServiceItemViewModel>>(serviceItemsDTO)));
+    }
+}
