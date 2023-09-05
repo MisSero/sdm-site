bafc47717ff98863c77770442ec17309557b2c34
MisSero <misha555835@gmail.com>
2023-09-05 09:46:18 +0300

    update HomeController, add Index, Contacts Views

--- SDM.WEB/Controllers/HomeController.cs
+++ SDM.WEB/Controllers/HomeController.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SDM.BLL.DTO;
+using SDM.BLL.Interfaces;
+using SDM.WEB.Models;
 
 namespace SDM.WEB.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly ITextFieldsService _textFieldsService;
+
+        public HomeController(ITextFieldsService textFieldsService)
         {
-            return View();
+            _textFieldsService = textFieldsService;
         }
-    }
+
+        public async Task<ActionResult> Index()
+        {
+            var textField = await _textFieldsService.GetTextFildByCodeWord("PageIndex");
+            var mapper = new MapperConfiguration(cfg =>
+                cfg.CreateMap<TextFieldDTO, TextFieldViewModel>()).CreateMapper();
+
+            return View(mapper.Map<TextFieldDTO, TextFieldViewModel>(textField));
+        }
+
+		public async Task<ActionResult> Contacts()
+		{
+			var textField = await _textFieldsService.GetTextFildByCodeWord("PageContacts");
+			var mapper = new MapperConfiguration(cfg =>
+				cfg.CreateMap<TextFieldDTO, TextFieldViewModel>()).CreateMapper();
+
+			return View(mapper.Map<TextFieldDTO, TextFieldViewModel>(textField));
+		}
+	}
 }
